1f3a0bd7aed954204fbcf2c07e17019cf491413b
Fraser Thompson <fraserthompson0@gmail.com>
2018-01-03 13:19:03 +1300

    Don't get clever

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@ FROM nginx:alpine
 
 COPY nginx.conf /etc/nginx/nginx.conf
 COPY docker-entrypoint.sh /docker-entrypoint.sh
+COPY usrlocalbin /usr/local/bin
 
 RUN apk add --update --no-cache openssl certbot && \
     mkdir -p /etc/nginx/ssl/live/scrapers.doctorpricer.co.nz && \
-    chmod +x /docker-entrypoint.sh
+    chmod +x /docker-entrypoint.sh && \
+    chmod +x /usr/local/bin/certbot-get
 
 ENTRYPOINT [ "/docker-entrypoint.sh" ]
 CMD ["nginx", "-g", "daemon off;"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,24 +1,14 @@
 #! /bin/sh
 
-if [ "$ENV" == "staging"]; then
+cd /etc/nginx/ssl/live/scrapers.doctorpricer.co.nz
+PASS=$(openssl rand -base64 12)
 
-    cd /etc/nginx/ssl/live/scrapers.doctorpricer.co.nz
-    PASS=$(openssl rand -base64 12)
+openssl genrsa -des3 -passout pass:${PASS}  -out server.pass.key 2048
+openssl rsa -passin pass:${PASS} -in server.pass.key -out privkey.pem
 
-    openssl genrsa -des3 -passout pass:${PASS}  -out server.pass.key 2048
-    openssl rsa -passin pass:${PASS} -in server.pass.key -out privkey.pem
+rm server.pass.key
 
-    rm server.pass.key
-
-    openssl req -new -key privkey.pem -out doctorpricer.co.nz.csr -subj "/C=NZ/ST=Otago/L=Dunedin/O=doctorpricer.co.nz/OU=IT Department/CN=*.doctorpricer.co.nz"
-    openssl x509 -req -days 365 -in doctorpricer.co.nz.csr -signkey privkey.pem -out fullchain.pem
-
-else 
-
-    if [ ! -d /etc/letsencrypt/live/scrapers.doctorpricer.co.nz ]; then
-        /usr/bin/certbot certonly --non-interactive --agree-tos --email doctorpricernz@gmail.com --standalone -d scrapers.doctorpricer.co.nz
-    fi
-
-fi
+openssl req -new -key privkey.pem -out doctorpricer.co.nz.csr -subj "/C=NZ/ST=Otago/L=Dunedin/O=doctorpricer.co.nz/OU=IT Department/CN=*.doctorpricer.co.nz"
+openssl x509 -req -days 365 -in doctorpricer.co.nz.csr -signkey privkey.pem -out fullchain.pem
 
 exec "$@"
